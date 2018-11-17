ea00c77290b042aca156cbae6eaebb114d8f09ec
Clarence Kwong <clarencekwong7@gmail.com>
2018-11-17 21:57:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,20 @@
 # Add  code here!
 
 def prime?(num)
+  if num == 2
+    return True
+  if num == 3
+    return True
+  if n % 2 == 0
+    return False
+  if n % 3 == 0
+    return False
+  i = 5
+  w = 2
   
+  while (i * i) <= num 
+    if n % i == 0
+      return False
+    i += w
+    w = 6 - w
+  return True
