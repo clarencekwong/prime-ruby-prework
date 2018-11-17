06b17f0ff702731c2ca32f2b53029af6a52fa6d6
Clarence Kwong <clarencekwong7@gmail.com>
2018-11-17 22:01:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,10 +7,10 @@ def prime?(num)
   if num == 3
     return true
   end
-  if n % 2 == 0
+  if num % 2 == 0
     return false
   end
-  if n % 3 == 0
+  if num % 3 == 0
     return false
   end
   i = 5
